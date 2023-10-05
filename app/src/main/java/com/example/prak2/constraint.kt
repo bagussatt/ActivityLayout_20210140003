9752b19c36244425238b76e4180e2d4765ae44bf
Bagus Satrio <bagus.satrio.ft21@mail.umy.ac.id>
2023-10-05 09:10:44 +0700

    first commit

---
+++ app/src/main/java/com/example/prak2/constraint.kt
@@ -0,0 +1,11 @@
+package com.example.prak2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class constraint : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_constraint)
+    }
+}
---
+++ app/src/main/java/com/example/prak2/tableprofile.kt
@@ -0,0 +1,11 @@
+package com.example.prak2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class tableprofile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tableprofile)
+    }
+}
